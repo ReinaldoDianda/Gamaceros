28672bef2a77d5c18b36976cc88bcb529c7ed4d6
beicod <beicodcorp@gmail.com>
2023-10-24 16:41:25 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const counters = document.querySelectorAll('.number-increment-animation')
+const speed = 300
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target')
+    const count = +counter.innerText
+
+    const inc = target / speed
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + inc)
+      setTimeout(updateCount, 1)
+    } else {
+      counter.innerText = target
+    }
+  }
+  updateCount()
+})
+
+const acc = document.getElementsByClassName('accordion')
+let i
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+    const panel = this.nextElementSibling
+    if (panel.style.display === 'block') {
+      panel.style.display = 'none'
+    } else {
+      panel.style.display = 'block'
+    }
+  })
+}
